4f5dc61c393123240cdc6eba02181572f57fd349
Ali Sheraz <alisheraz7777@gmail.com>
2020-02-24 04:31:50 -0800

    Initial commit

---
+++ app/src/main/java/com/buddytrainers/bottomnavfragmentapp/Fragments/HomeFragment.java
@@ -0,0 +1,29 @@
+package com.buddytrainers.bottomnavfragmentapp.Fragments;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.buddytrainers.bottomnavfragmentapp.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
